8d6825bbc4c9fb2bbeb03415494a4e1e3ac9673f
Bianca Milea <alumil2013@ieselcaminas.org>
2022-10-04 22:11:12 +0200

    Example 8

--- src/Example8.java
+++ src/Example8.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Example8 {
+import java.util.Scanner;
+class Example8 {
+    public static void main(String[] argv) {
+        int age;
+        System.out.println("Enter your age:");
+        //Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        age = inputValue.nextInt();
+        if (age >= 18) {
+            System.out.println("You have the legal age");
+        }
+    }
 }
